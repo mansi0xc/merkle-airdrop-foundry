0d833755c5fca9e552f0b8f3e502aaa87c62415b
mansi0xc <mansi.tibrewal76@gmail.com>
2025-09-01 02:15:49 +0530

    cereal

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MerkleAirdrop {
+    using SafeERC20 for IERC20;
+    // we have a list of address which can claim the airdrop tokens
+    error MerkleAirdrop__InvalidProof();
+
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    event Claim(address account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, address airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = IERC20(airdropToken);
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // calculate using account and amount, the hash -> leaf node
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
+}
