e68fcf3676bf75c296b708c7371f47d54d275677
mansi0xc <mansi.tibrewal76@gmail.com>
2025-10-02 23:33:30 +0530

    previous work

---
+++ script/GenerateInput.s.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+
+contract GenerateInput is Script {
+    string constant FILE_PATH = "script/target/input.json";
+
+    function run() external {
+        // Define airdrop data
+        string[] memory types = new string[](2);
+        types[0] = "address";
+        types[1] = "uint";
+
+        uint256 amount = 2500 * 1e18; // Example amount​
+        address[] memory whitelist = new address[](4);
+        whitelist[0] = 0x6CA6D1e2D5347bfaB1d91E883F1915560E891290; 
+                    // 0x6CA6d1e2D5347Bfab1d91e883F1915560e891290;
+        whitelist[1] = 0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B; // Example addresses
+        whitelist[2] = 0x1Db3439a222C519ab44bb1144fC28167b4Fa6EE6;
+        whitelist[3] = 0x0e466e7519A469f20168796a0807b758a2339791;
+                    // 0x0E466e7519A469F20168796A0807B758A2339791;
+
+        string memory json = createJSON(types, whitelist, amount);
+        vm.writeFile(FILE_PATH, json);
+        console.log("Successfully wrote input.json to %s", FILE_PATH);
+    }
+
+    function createJSON(
+        string[] memory types,
+        address[] memory whitelist,
+        uint256 amount
+    ) internal pure returns (string memory) {
+        string memory json = "{";
+
+        // Add types
+        json = string.concat(json, "\"types\": [");
+        for (uint i = 0; i < types.length; i++) {
+            json = string.concat(json, "\"", types[i], "\"");
+            if (i < types.length - 1) {
+                json = string.concat(json, ", ");
+            }
+        }
+        json = string.concat(json, "], ");
+
+        // Add count
+        json = string.concat(
+            json,
+            "\"count\": ",
+            vm.toString(whitelist.length),
+            ", "
+        );
+
+        // Add values
+        json = string.concat(json, "\"values\": {");
+        for (uint i = 0; i < whitelist.length; i++) {
+            json = string.concat(
+                json,
+                "\"",
+                vm.toString(i),
+                "\": {",
+                "\"0\": \"",
+                vm.toString(whitelist[i]),
+                "\", ",
+                "\"1\": \"",
+                vm.toString(amount),
+                "\"}"
+            );
+            if (i < whitelist.length - 1) {
+                json = string.concat(json, ", ");
+            }
+        }
+        json = string.concat(json, "}}");
+        return json;
+    }
+}
--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -1,7 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {CivetToken} from "../src/CivetToken.sol";
 
-contract MerkleAirdropis is Test {}
+contract MerkleAirdropTest is Test {
+    MerkleAirdrop public airdrop;
+    CivetToken public token;
+
+    bytes32 public ROOT = 0x59162885dcb346508731ca48791c029f931d71d0d576f65fadf2a430fc890236;
+    uint256 public AMOUNT_TO_CLAIM = 25 * 1e18;
+    uint256 public AMOUNT_TO_SEND = AMOUNT_TO_CLAIM * 4;
+    bytes32 proofOne = 0x9e10faf86d92c4c65f81ac54ef2a27cc0fdf6bfea6ba4b1df5955e47f187115b;
+    bytes32 proofTwo = 0x8c1fd7b608678f6dfced176fa3e3086954e8aa495613efcd312768d41338ceab;
+    bytes32[] public PROOF = [proofOne, proofTwo];
+    address user;
+    uint256 userPrivKey;
+
+    function setUp() public {
+        token = new CivetToken();
+        airdrop = new MerkleAirdrop(ROOT, address(token));
+        token.mint(token.owner(), AMOUNT_TO_SEND);
+        token.transfer(address(airdrop), AMOUNT_TO_SEND);
+        (user, userPrivKey) = makeAddrAndKey("user");
+    }
+
+    function testUsersCanClaim() public {
+        console.log("User address:", user);
+        console.log("User private key:", userPrivKey);
+
+        uint256 startingBalance = token.balanceOf(user);
+        console.log("User starting balance:", startingBalance);
+
+        vm.prank(user);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("User ending balance:", endingBalance);
+        assertEq(endingBalance - startingBalance, AMOUNT_TO_CLAIM);
+    }
+}
